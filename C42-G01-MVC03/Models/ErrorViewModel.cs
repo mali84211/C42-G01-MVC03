9caf1c061c1f47a94c5d0ee08573610d35eff7cf
mali84211 <mali84211@gmail.com>
2024-09-12 20:27:07 +0300

    Add project files.

---
+++ C42-G01-MVC03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C42_G01_MVC03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
